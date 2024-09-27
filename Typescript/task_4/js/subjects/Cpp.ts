1e170782fd3ec5cef28f772d70dcb5fe91917948
arycodes0 <alvarezaryam@gmail.com>
2024-09-27 15:10:32 -0400

    fix: Fixing code structure errors

--- Typescript/task_4/js/subjects/Cpp.ts
+++ Typescript/task_4/js/subjects/Cpp.ts
@@ -2,15 +2,16 @@ namespace Subjects {
     export interface Teacher {
         experienceTeachingC?: number; 
     }
-    class Cpp extends Subject {
-        getRequirements(): string {
+    export class Cpp extends Subject {
+        getRequirements() {
             return 'Here is the list of requirements for Cpp';
         }
-        getAvailableTeacher(): string {
-            if (this.teacher.experienceTeachingC !== null) {
-                return `Available Teacher: ${this.teacher.firstName}`
+
+        getAvailableTeacher() {
+            if (this.teacher && this.teacher.experienceTeachingC > 0) {
+                return 'Available Teacher: ' + this.teacher.firstName;
             } else {
-                return "No available teacher";
+                return 'No available teacher';
             }
         }
     }
