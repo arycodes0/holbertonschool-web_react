18a1f52b313694a15ffbd1d77494815db78c5ff3
arycodes0 <alvarezaryam@gmail.com>
2024-09-27 15:23:12 -0400

    fix: Fixing code structure errors and returns

--- Typescript/task_5/js/main.ts
+++ Typescript/task_5/js/main.ts
@@ -1,19 +1,21 @@
 
-interface MajorCredits {
+export interface MajorCredits {
+    _majorCreditBrand: void;
     credits: number;
 }
 
-interface MinorCredits {
+export interface MinorCredits {
+    _minorCreditBrand: void;
     credits: number;
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
-    return { credits: subject1.credits + subject2.credits };
-};
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return { credits: subject1.credits + subject2.credits } as MajorCredits;
+}
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
-    return { credits: subject1.credits + subject2.credits };
-};
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return { credits: subject1.credits + subject2.credits } as MinorCredits;
+}
 
 const major1: MajorCredits = { credits: 3 };
 const major2: MajorCredits = { credits: 4 };
